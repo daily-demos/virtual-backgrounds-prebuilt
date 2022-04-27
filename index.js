98336fca7dc0f001325f4a2029e6c80e0adb09e9
Liza <liza@daily.co>
2022-04-27 17:11:39 +0200

    cleanup, prettier

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ let callFrame;
 
 const backgroundPrefix = "backgrounds";
 const backgroundsData = [
-    {
-        path: "cafe-table-with-coffee.jpg",
-        alt: "A round coffee table with a cup of coffee",
-    },
-    {
-        path: "northern-lights.jpg",
-        alt: "Northern lights",
-    },
-    {
-        path: "outdoor-cafe-with-cat.jpg",
-        alt: "An outdoor cafe with a cat",
-    },
-    {
-        path: "rocky-forest.jpg",
-        alt: "A rocky forest",
-    },
-    {
-        path: "snowy-mountains.jpg",
-        alt: "Snowy mountains",
-    },
-    {
-        path: "sunny-beach.jpg",
-        alt: "A sunny beach",
-    },
-    {
-        path: "warm-restaurant.jpg",
-        alt: "A warm restaurant",
-    },
+  {
+    path: "cafe-table-with-coffee.jpg",
+    alt: "A round coffee table with a cup of coffee",
+  },
+  {
+    path: "northern-lights.jpg",
+    alt: "Northern lights",
+  },
+  {
+    path: "outdoor-cafe-with-cat.jpg",
+    alt: "An outdoor cafe with a cat",
+  },
+  {
+    path: "rocky-forest.jpg",
+    alt: "A rocky forest",
+  },
+  {
+    path: "snowy-mountains.jpg",
+    alt: "Snowy mountains",
+  },
+  {
+    path: "sunny-beach.jpg",
+    alt: "A sunny beach",
+  },
+  {
+    path: "warm-restaurant.jpg",
+    alt: "A warm restaurant",
+  },
 ];
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -38,12 +38,12 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 function initBackgrounds() {
-    const backgrounds = document.getElementById("backgrounds");
-    const resetBtn = backgrounds.getElementsByTagName("button")[0];
-    resetBtn.onclick = () => {
-      resetBackground();
-    };
-    loadBackgrounds();
+  const backgrounds = document.getElementById("backgrounds");
+  const resetBtn = backgrounds.getElementsByTagName("button")[0];
+  resetBtn.onclick = () => {
+    resetBackground();
+  };
+  loadBackgrounds();
 }
 
 function initCall() {
@@ -61,56 +61,54 @@ function initCall() {
       console.warn("nonfatal error:", e);
     })
     .on("started-camera", () => {
-        const backgrounds = document.getElementById("backgrounds");
-        backgrounds.classList.remove("hidden");
+      const backgrounds = document.getElementById("backgrounds");
+      backgrounds.classList.remove("hidden");
     })
     .on("left-meeting", () => {
       initCall();
     });
 
   // TODO: Replace the following URL with your own room URL.
- callFrame.join({ url: "https://lizashul.daily.co/testing3"});
+  callFrame.join({ url: "https://<your-domain>.daily.co/<room-name>" });
 }
 
-function setBackground(imgPath) {   
-    callFrame.updateInputSettings({
-      video: {
-        processor: {
-          type: "background-image",
-          config: {
-            source: imgPath,
-          },
+function setBackground(imgPath) {
+  callFrame.updateInputSettings({
+    video: {
+      processor: {
+        type: "background-image",
+        config: {
+          source: imgPath,
         },
       },
-    });
-}  
+    },
+  });
+}
 
 function resetBackground() {
-    callFrame.updateInputSettings({
-        video: {
-          processor: {
-            type: "none",
-          },
-        },
-      });
+  callFrame.updateInputSettings({
+    video: {
+      processor: {
+        type: "none",
+      },
+    },
+  });
 }
 
 async function loadBackgrounds() {
-    const bgImages = document.getElementById("bgImages");
+  const bgImages = document.getElementById("bgImages");
 
-    for (let i = 0; i < backgroundsData.length; i++) {
-        const data = backgroundsData[i];
-        const imgPath = `${window.location.protocol}//${window.location.host}/${backgroundPrefix}/${data.path}`;
+  for (let i = 0; i < backgroundsData.length; i++) {
+    const data = backgroundsData[i];
+    const imgPath = `${window.location.protocol}//${window.location.host}/${backgroundPrefix}/${data.path}`;
 
-        const img = document.createElement("img");
-        img.src = imgPath;
+    const img = document.createElement("img");
+    img.src = imgPath;
 
-        img.onclick = () => {
-          setBackground(imgPath);
-        };
-        img.alt = data.alt;
-        bgImages.appendChild(img);
-    }
-  
+    img.onclick = () => {
+      setBackground(imgPath);
+    };
+    img.alt = data.alt;
+    bgImages.appendChild(img);
+  }
 }
-
