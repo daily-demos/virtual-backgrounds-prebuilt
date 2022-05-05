bd69ae25408e76a1cb50ac1b5cc8c6dfa1e69a76
lizadaily <93514641+lizadaily@users.noreply.github.com>
2022-05-05 12:29:26 +0200

    Virtual backgrounds demo (#1)

---
+++ README.md
@@ -0,0 +1,23 @@
+# Daily Prebuilt virtual backgrounds demo
+
+This demo showcases usage of custom virtual backgrounds with [Daily Prebuilt](https://www.daily.co/blog/prebuilt-ui/).
+
+![homescreen](./screenshot.png)
+
+## Prerequisites
+
+- [Sign up for a Daily account](https://dashboard.daily.co/signup).
+
+## How the demo works
+
+This demo embeds the Daily Prebuilt iframe into a vanilla JavaScript application. It then loads a set of custom background images for the user to select from, allowing them to update their background in the video call.
+
+## Running locally
+
+1. Replace the room URL in `index.js` with the URL of your own room.
+1. Install dependencies `npm i`
+1. Run `npm start`
+
+## Contributing and feedback
+
+Let us know how experimenting with this demo goes! Feel free to reach out to us any time at `help@daily.co`.
---
+++ index.js
@@ -0,0 +1,123 @@
+let callFrame;
+
+const backgroundPrefix = "backgrounds";
+const backgroundsData = [
+  {
+    path: "cafe-table-with-coffee.jpg",
+    alt: "A round coffee table with a cup of coffee",
+  },
+  {
+    path: "northern-lights.jpg",
+    alt: "Northern lights",
+  },
+  {
+    path: "outdoor-cafe-with-cat.jpg",
+    alt: "An outdoor cafe with a cat",
+  },
+  {
+    path: "rocky-forest.jpg",
+    alt: "A rocky forest",
+  },
+  {
+    path: "snowy-mountains.jpg",
+    alt: "Snowy mountains",
+  },
+  {
+    path: "sunny-beach.jpg",
+    alt: "A sunny beach",
+  },
+  {
+    path: "warm-restaurant.jpg",
+    alt: "A warm restaurant",
+  },
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+  initCall();
+  initBackgrounds();
+});
+
+function initBackgrounds() {
+  const backgrounds = document.getElementById("backgrounds");
+  const resetBtn = backgrounds.getElementsByTagName("button")[0];
+  resetBtn.onclick = () => {
+    resetBackground();
+  };
+  loadBackgrounds();
+}
+
+function initCall() {
+  const container = document.getElementById("call");
+
+  callFrame = DailyIframe.createFrame(container, {
+    showLeaveButton: true,
+    iframeStyle: {
+      position: "fixed",
+      width: "500px",
+      height: "550px",
+      border: "1px solid #e6eaef",
+      borderRadius: "6px",
+      boxShadow: `0 1px 2px rgba(0, 0, 0, 0.02), 0 2px 4px rgba(0, 0, 0, 0.02),
+      0 4px 8px rgba(0, 0, 0, 0.02), 0 8px 16px rgba(0, 0, 0, 0.02),
+      0 16px 32px rgba(0, 0, 0, 0.02)`,
+    },
+  })
+    .on("nonfatal-error", (e) => {
+      console.warn("nonfatal error:", e);
+    })
+    .on("started-camera", () => {
+      const backgrounds = document.getElementById("backgrounds");
+      backgrounds.classList.remove("hidden");
+    })
+    .on("left-meeting", () => {
+      initCall();
+    });
+
+  // TODO: Replace the following URL with your own room URL.
+  callFrame.join({ url: "https://<your-domain>.daily.co/<room-name>" });
+}
+
+function setBackground(imgPath) {
+  callFrame.updateInputSettings({
+    video: {
+      processor: {
+        type: "background-image",
+        config: {
+          source: imgPath,
+        },
+      },
+    },
+  });
+}
+
+function resetBackground() {
+  callFrame.updateInputSettings({
+    video: {
+      processor: {
+        type: "none",
+      },
+    },
+  });
+}
+
+async function loadBackgrounds() {
+  const bgImages = document.getElementById("bgImages");
+
+  for (let i = 0; i < backgroundsData.length; i++) {
+    const data = backgroundsData[i];
+
+    const btn = document.createElement("button");
+    btn.className = "bg";
+    const imgPath = `${window.location.protocol}//${window.location.host}/${backgroundPrefix}/${data.path}`;
+
+    const img = document.createElement("img");
+    img.src = imgPath;
+    img.alt = data.alt;
+
+    btn.onclick = () => {
+      setBackground(imgPath);
+    };
+    btn.appendChild(img);
+    bgImages.appendChild(btn);
+  }
+}
